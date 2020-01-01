784893fd71ef0ab21eb32186880b83e07bf455fc
David Wightman <david@davidwightman.org>
2019-12-31 22:21:37 -0500

    added styles and finished project

--- timer.js
+++ timer.js
@@ -16,10 +16,10 @@ class Timer {
 
     start = () => {
         if (this.onStart) {
-            this.onStart();
+            this.onStart(this.timeRemaining);
         }
         this.tick()
-        this.interval = setInterval(this.tick, 1000)
+        this.interval = setInterval(this.tick, 20)
     }
 
     tick = () => {
@@ -30,9 +30,9 @@ class Timer {
             }
         } else {
             
-            this.timeRemaining = this.timeRemaining - 1;
+            this.timeRemaining = this.timeRemaining - .02;
             if (this.onTick) {
-                this.onTick();
+                this.onTick(this.timeRemaining);
             }
         }
     }
@@ -42,7 +42,7 @@ class Timer {
     }
 
     set timeRemaining(time) {
-        this.durationInput.value = time;
+        this.durationInput.value = time.toFixed(2);
     }
 
     pause = () => {
